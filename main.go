46a8bcb633cefc1bdac094d6b565176cf8bb85e8
Harsh Raj <harsh.smartters@gmail.com>
2024-03-24 20:50:40 +0530

    refactored models

--- main.go
+++ main.go
@@ -11,18 +11,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Movie struct {
-	ID       string    `json:"id"`
-	Isbn     string    `json:"isbn"`
-	Title    string    `json:"title"`
-	Director *Director `json:"director"`
-}
-
-type Director struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-}
-
 var movies []Movie
 
 func getMovies(w http.ResponseWriter, r *http.Request) {
